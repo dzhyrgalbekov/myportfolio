b3c746233e1ff6076b6ac20229eb07792289235b
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-10-28 09:34:16 +0600

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
  	</h1>My name is Kubanych</h1>
 	<h2> This is my homepage </h2>
 	<h3>This is my city</h3>
-	<img scr= "images.jpeg"  width="600" height="500">
+	<img scr= "images.jpeg">
 
 	<h3>My social media:</h3>
 	<div>Instagram: <a href="https://instagram.com/kuba_dzh?utm_medium=copy_link" target="_blank">@kuba_dzh</a></div>
