63720d77848890d91fed120d4bb128ed550552b8
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-10-28 09:45:11 +0600

    Update index.php

--- index.php
+++ index.php
@@ -48,8 +48,7 @@
  	</h1>My name is Kubanych</h1>
 	<h2> This is my homepage </h2>
 	<h3>This is my city</h3>
-	<img scr= "10-things-to-do-bishkek-kyrgyzstan-main-image-900.jpg">
-
+	<img src="10-things-to-do-bishkek-kyrgyzstan-main-image-900.jpg" alt="Trulli" width="500" height="333">
 	<h3>My social media:</h3>
 	<div>Instagram: <a href="https://instagram.com/kuba_dzh?utm_medium=copy_link" target="_blank">@kuba_dzh</a></div>
 
