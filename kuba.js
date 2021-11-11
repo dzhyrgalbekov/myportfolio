971e44cb676dc174cc312b1d905dc51a0353828e
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-11-10 23:46:53 -0500

    Update kuba.js

--- kuba.js
+++ kuba.js
@@ -1,40 +1,65 @@
 	//Validtion Code For Inputs
 
     var email = document.forms['form']['email'];
-    var password = document.forms['form']['password'];
-    
-    var email_error = document.getElementById('email_error');
-    var pass_error = document.getElementById('pass_error');
-    
-    email.addEventListener('textInput', email_Verify);
-    password.addEventListener('textInput', pass_Verify);
-    
-    function validated(){
-        if (email.value.length > 0) {
-            email.style.border = "1px solid red";
-            email_error.style.display = "block";
-            email.focus();
-            return false;
-        }
-        if (password.value.length > 0) {
-            password.style.border = "1px solid red";
-            pass_error.style.display = "block";
-            password.focus();
-            return false;
-        }
-    
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+    email_error.style.display = "block";
+    email.focus();
+    return false;
+}
+if (password.value.length < 8) {
+    password.style.border = "1px solid red";
+    pass_error.style.display = "block";
+    password.focus();
+    return false;
+}
+
+}
+function email_Verify(){
+if (email.value.length >= 8) {
+    email.style.border = "1px solid silver";
+    email_error.style.display = "none";
+    return true;
+}
+}
+function pass_Verify(){
+if (password.value.length >= 5) {
+    password.style.border = "1px solid silver";
+    pass_error.style.display = "none";
+    return true;
+}
+}if (email.value.length > 4) {
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
     }
-    function email_Verify(){
-        if (email.value.length kubanych.zh@gmail.com) {
-            email.style.border = "1px solid silver";
-            email_error.style.display = "none";
-            return true;
-        }
+    if (password.value.length < 8) {
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
     }
-    function pass_Verify(){
-        if (password.value.length kubanych.zh@gmail.com) {
-            password.style.border = "1px solid silver";
-            pass_error.style.display = "none";
-            return true;
-        }
+
+}
+function email_Verify(){
+    if (email.value.length >= 8) {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
     }
+}
+function pass_Verify(){
+    if (password.value.length >= 5) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
+}
