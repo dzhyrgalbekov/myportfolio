9b3df78123725a5c78280bf0e0990a68bfe733a7
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-11-18 12:51:16 +0600

    Update kuba.js

--- kuba.js
+++ kuba.js
@@ -1,40 +1,16 @@
-	//Validtion Code For Inputs
+function validate()
+{
 
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
-
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
-
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
-
-function validated(){
-	if (email.value.length < 9) {
-		email.style.border = "1px solid red";
-		email_error.style.display = "block";
-		email.focus();
-		return false;
-	}
-	if (password.value.length < 6) {
-		password.style.border = "1px solid red";
-		pass_error.style.display = "block";
-		password.focus();
-		return false;
-	}
-
+var email = document.gitElementByID['email'].value;
+var password = document.gitElementByID['password'].value;
+if(email=="kuba"&& password=="2004")
+{
+    alert("login succesfully")
+    return false;
 }
-function email_Verify(){
-	if (email.value.length >= 8) {
-		email.style.border = "1px solid silver";
-		email_error.style.display = "none";
-		return true;
-	}
+else
 }
-function pass_Verify(){
-	if (password.value.length >= 5) {
-		password.style.border = "1px solid silver";
-		pass_error.style.display = "none";
-		return true;
-	}
+alert("login failed")
 }
+}
+
