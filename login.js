1a9874d53c37b6d98eb1b63d9546392831d5cfea
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-11-18 12:56:27 +0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,15 @@
+
+function validate()
+{
+
+var email = document.gitElementByID['email'].value;
+var password = document.gitElementByID['password'].value;
+if(email=="kuba"&& password=="2004")
+{
+    alert("login succesfully")
+    return false;
+}
+else
+}
+alert("login failed")
+}
