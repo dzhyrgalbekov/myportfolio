9f57f42eeb35ff9ab14c7c058f19a72bac595cd4
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-11-18 13:01:08 +0600

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,17 @@
 
 function validate()
 {
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
 
-var email = document.gitElementByID['email'].value;
-var password = document.gitElementByID['password'].value;
+var email_error = document.getElementById('email_error').value;
+var pass_error = document.getElementById('pass_error').value;
+
 if(email=="kuba"&& password=="2004")
 {
     alert("login succesfully")
     return false;
-}
+{
 else
 }
 alert("login failed")
