201c700008c2189a9cd89d636c9302d4e39e55e2
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-11-18 13:08:08 +0600

    Update and rename login.js to login2.js

---
+++ login2.js
@@ -0,0 +1,15 @@
+
+function validate()
+{
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+
+if(email=="kuba"&& password=="2004")
+{
+    alert("login succesfully")
+    return false;
+{
+else
+}
+alert("login failed")
+}
