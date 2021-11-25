51f8128a98af759b3c31c08083d0cf8e457d43a9
Kuba7443 <91646666+Kuba7443@users.noreply.github.com>
2021-11-25 09:51:35 +0600

    Update login.php

--- login.php
+++ login.php
@@ -110,7 +110,7 @@ myInput.onblur = function() {
 // When the user starts to type something inside the password field
 myInput.onkeyup = function() {
   // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
+  var lowerCaseLetters = /[uba]/g;
   if(myInput.value.match(lowerCaseLetters)) {  
     letter.classList.remove("invalid");
     letter.classList.add("valid");
@@ -120,7 +120,7 @@ myInput.onkeyup = function() {
   }
   
   // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
+  var upperCaseLetters = /[K]/g;
   if(myInput.value.match(upperCaseLetters)) {  
     capital.classList.remove("invalid");
     capital.classList.add("valid");
@@ -130,7 +130,7 @@ myInput.onkeyup = function() {
   }
 
   // Validate numbers
-  var numbers = /[0-9]/g;
+  var numbers = /[2004]/g;
   if(myInput.value.match(numbers)) {  
     number.classList.remove("invalid");
     number.classList.add("valid");
